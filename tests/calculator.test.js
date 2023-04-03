6795a97ebdd06d5c6b0ba3eecce110541abfe379
fprogs <60819474+fprogs@users.noreply.github.com>
2023-04-02 21:52:22 -0400

    Fix spelling

--- tests/calculator.test.js
+++ tests/calculator.test.js
@@ -28,11 +28,11 @@ test("Addition with 0", () => {
   expect(Calculator.add(1, 0)).toBe(1);
 });
 
-test("Subtact with two positive integers", () => {
+test("Subtract with two positive integers", () => {
   expect(Calculator.subtract(1, 2)).toBe(-1);
 });
 
-test("Subtact with two negative integers", () => {
+test("Subtract with two negative integers", () => {
   expect(Calculator.subtract(-1, -2)).toBe(1);
 });
 
@@ -56,4 +56,30 @@ test("Subtract with 0", () => {
   expect(Calculator.subtract(1, 0)).toBe(1);
 });
 
+test("Subtract with two positive integers", () => {
+  expect(Calculator.subtract(1, 2)).toBe(-1);
+});
 
+test("Subtract with two negative integers", () => {
+  expect(Calculator.subtract(-1, -2)).toBe(1);
+});
+
+test("Subtract with a positive and a negative integer", () => {
+  expect(Calculator.subtract(-1, 2)).toBe(-3);
+});
+
+test("Subtract with two floating point numbers", () => {
+  expect(Calculator.subtract(1.5, 2.2)).toBeCloseTo(-0.7);
+});
+
+test("Subtract with two negative integer", () => {
+  expect(Calculator.subtract(-1.5, -2.2)).toBeCloseTo(0.7);
+});
+
+test("Subtract with a positive and a negative floating point number", () => {
+  expect(Calculator.subtract(-1.5, 2.2)).toBeCloseTo(-3.7);
+});
+
+test("Subtract with 0", () => {
+  expect(Calculator.subtract(1, 0)).toBe(1);
+});
