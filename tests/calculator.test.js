99b1b86243220fe96e49a9194eea62758a06843a
fprogs <60819474+fprogs@users.noreply.github.com>
2023-04-02 22:16:17 -0400

    Add calculator.divide tests

--- tests/calculator.test.js
+++ tests/calculator.test.js
@@ -56,30 +56,30 @@ test("Subtract with 0", () => {
   expect(Calculator.subtract(1, 0)).toBe(1);
 });
 
-test("Subtract with two positive integers", () => {
-  expect(Calculator.subtract(1, 2)).toBe(-1);
+test("Division with two positive integers", () => {
+  expect(Calculator.divide(1, 2)).toBeCloseTo(0.5);
 });
 
-test("Subtract with two negative integers", () => {
-  expect(Calculator.subtract(-1, -2)).toBe(1);
+test("Division with two negative integers", () => {
+  expect(Calculator.divide(-1, -2)).toBeCloseTo(0.5);
 });
 
-test("Subtract with a positive and a negative integer", () => {
-  expect(Calculator.subtract(-1, 2)).toBe(-3);
+test("Division with a positive and a negative integer", () => {
+  expect(Calculator.divide(-1, 2)).toBe(-0.5);
 });
 
-test("Subtract with two floating point numbers", () => {
-  expect(Calculator.subtract(1.5, 2.2)).toBeCloseTo(-0.7);
+test("Division with two floating point numbers", () => {
+  expect(Calculator.divide(1.5, 2.2)).toBeCloseTo(0.68);
 });
 
-test("Subtract with two negative integer", () => {
-  expect(Calculator.subtract(-1.5, -2.2)).toBeCloseTo(0.7);
+test("Division with two negative integer", () => {
+  expect(Calculator.divide(-1.5, -2.2)).toBeCloseTo(0.68);
 });
 
-test("Subtract with a positive and a negative floating point number", () => {
-  expect(Calculator.subtract(-1.5, 2.2)).toBeCloseTo(-3.7);
+test("Division with a positive and a negative floating point number", () => {
+  expect(Calculator.divide(-1.5, 2.2)).toBeCloseTo(-0.68);
 });
 
-test("Subtract with 0", () => {
-  expect(Calculator.subtract(1, 0)).toBe(1);
+test("Division with 0", () => {
+  expect(Calculator.divide(1, 0)).toBe(Number.POSITIVE_INFINITY);
 });
